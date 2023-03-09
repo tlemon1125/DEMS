225ed0d5082d33fd898ffc64ac481c7caf5d80bc
Khew Jia Peng <jiapengkhew@gmail.com>
2023-03-09 16:54:27 +0800

    First Commit and Build Basic Structure For Front End and Back End

---
+++ resources/js/Pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import { Head } from "@inertiajs/react";
+
+export default function Dashboard() {
+    return (
+        <>
+            <Head title="Dashboard" />
+            <div>Dashboard</div>
+        </>
+    );
+}
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return Redirect::route('login');
+});
+
+Route::get('/login', function() {
+    return Inertia::render('Auth/Login');
+})->name('login');
